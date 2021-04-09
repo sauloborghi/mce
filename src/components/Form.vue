648c4f8df2d7fc27a4d8a780e72ca87e36ed39e8
Saulo Borghi <sauloborghi@live.com>
2021-04-08 21:43:58 -0300

    o

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,4 +1,5 @@
 <template>
+<div class="card">
   <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Email address</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
@@ -7,6 +8,16 @@
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
 </div>
 </template>
+
+<style scoped>
+  .dcard {
+    margin-top: 10px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+  </style>
+  
 <script>
 export default {
   name: 'Form',
