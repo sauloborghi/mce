abbf249261fe5a557dd639dd5c76c745c5496db5
Saulo Borghi <sauloborghi@live.com>
2021-04-09 00:30:30 -0300

    0.1

--- src/components/Rodape.vue
+++ src/components/Rodape.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer class="footer mt-auto py-3 bg-light">
+  <footer class="footer mt-auto py-3 bg-light rodape">
   <div class="container">
-    <span class="text-muted rodape">Todos os diteitos reservados. ©</span>
+    <span class="text-muted">Todos os diteitos reservados. ©</span>
   </div>
 </footer>
 </template>
@@ -10,7 +10,7 @@
   .rodape {
     background-color: #17a2b8;
     text-align: center;
-    color: white;
+    color: white!important;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px; 
   }
