ca1f1cb87f32147f85dbf1f9a58c10c41aa1f23f
Saulo Borghi <sauloborghi@live.com>
2021-04-08 22:12:51 -0300

    y

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -12,7 +12,7 @@
   <br>
   <label for="exampleFormControlInput1" class="form-label">Email address</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-  <button type="submit" class="btn btn-success" right>Enviar</button>
+  <button type="submit" class="btn btn-success mb-3 bu" right>Enviar</button>
 </div>
 </template>
 
@@ -28,6 +28,9 @@
     margin-left: 20px;
     margin-right: 20px;
     margin-bottom: 10px;
+  }
+  .bu{
+      padding-left: 20px;
   }
   </style>
   
