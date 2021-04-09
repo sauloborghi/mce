194dc699370e06a0f4457df58817b70f70e98952
Saulo Borghi <sauloborghi@live.com>
2021-04-08 22:52:59 -0300

    a

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="dcard">
+  <div>
   <b-card-group>
-    <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+    <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top>
       <b-card-text>
         This is a wider card with supporting text below as a natural lead-in to additional content.
         This content is a little bit longer.
@@ -11,7 +12,7 @@
       </template>
     </b-card>
 
-    <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+    <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top>
       <b-card-text>
         This card has supporting text below as a natural lead-in to additional content.
       </b-card-text>
@@ -20,7 +21,7 @@
       </template>
     </b-card>
 
-    <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+    <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top>
       <b-card-text>
         This is a wider card with supporting text below as a natural lead-in to additional content.
         This card has even longer content than the first to show that equal height action.
@@ -30,6 +31,7 @@
       </template>
     </b-card>
   </b-card-group>
+</div>
 </div>
 </template>
 
